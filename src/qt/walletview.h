4b25ef960d0ed88bd0997d73a71def13b5ec533a
tom-ua <stevenmai555@gmail.com>
2021-01-13 17:16:33 +0200

    gui: Fix unintialized WalletView::progressDialog

--- src/qt/walletview.h
+++ src/qt/walletview.h
@@ -73,7 +73,7 @@ private:
 
     TransactionView *transactionView;
 
-    QProgressDialog *progressDialog;
+    QProgressDialog* progressDialog{nullptr};
     const PlatformStyle *platformStyle;
 
 public Q_SLOTS:
