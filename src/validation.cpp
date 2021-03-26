c1f20fd90ba051e2db760f9444e4c8ada9bcb0ea
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2021-03-26 01:31:09 -0600

    Update validation.cpp

--- src/validation.cpp
+++ src/validation.cpp
@@ -256,7 +256,7 @@ static void CheckBlockIndex(const Consensus::Params& consensusParams);
 /** Constant stuff for coinbase transactions we create: */
 CScript COINBASE_FLAGS;
 
-const std::string strMessageMagic = "DarkCoin Signed Message:\n";
+const std::string strMessageMagic = "CSPN Signed Message:\n";
 
 // Internal stuff
 namespace {
